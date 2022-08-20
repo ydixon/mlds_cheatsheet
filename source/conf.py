55aa60d432036f7bd504d857ad539dc5bc2f98e6
unknown <ydixon@gmail.com>
2022-08-21 04:58:14 +0800

    fix sidebar

--- source/conf.py
+++ source/conf.py
@@ -13,15 +13,47 @@ author = 'Dickson Yu'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.mathjax',
+                'sphinx.ext.githubpages',
+                'recommonmark']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# -- Options for HTML output ----------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme" #'alabaster'
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+# html_theme_options = {}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': False
+#    'logo_only': True,
+}
+
+def setup(app):
+    #app.add_stylesheet('theme_overrides.css')
+    app.add_css_file('theme_overrides.css')
+
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
